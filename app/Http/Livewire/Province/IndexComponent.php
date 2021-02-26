3748f1b4f1ae7248afc87ef91f63a4f34803be58
Julian <ajarizaga@gmail.com>
2021-02-25 23:12:16 -0300

    componente creado

---
+++ app/Http/Livewire/Province/IndexComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Province;
+
+use Livewire\Component;
+
+class IndexComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.province.index-component');
+    }
+}
---
+++ resources/views/livewire/province/index-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Lista de provincias</h1>
+</div>
